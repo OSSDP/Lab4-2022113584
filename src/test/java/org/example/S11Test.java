d4e96f2b5e19f4b8f5d687f7c97e6a8a36d6a29a
Coltish <3252582845@qq.com>
2024-11-26 12:50:34 +0800

    yml2

--- src/test/java/org/example/S11Test.java
+++ src/test/java/org/example/S11Test.java
@@ -53,21 +53,21 @@ public class S11Test {
     public void testArrayWithAllPositive(){
         int[] nums={4,1,2,3,5};
         List<List<Integer>> result = solution11.threeSum(nums);
-        assertEquals(0, result.size());
+        assertEquals(0, 1);
     }
     //覆盖：输入数组长度大于三，并且全部为负数
     @Test
     public void testArrayWithAllNegative(){
         int[] nums={-3,-4,-1,-2,-5};
         List<List<Integer>> result = solution11.threeSum(nums);
-        assertEquals(0, result.size());
+        assertEquals(0, 1);
     }
     //覆盖：输入数组长度大于三，不止包含一种数，即不止全为正数、负数或0，但其中没有满足条件的三元组
     @Test
     public void testArrayWithNoTriple(){
         int[] nums={5,6,7,1,-2,3,4,8,9};
         List<List<Integer>> result = solution11.threeSum(nums);
-        assertEquals(0, result.size());
+        assertEquals(0, 1);
     }
     //覆盖：输入数组长度大于三，不止包含一种数，即不止全为正数、负数或0，但其中有满足条件的三元组
     @Test
